b18239c9e2bf3a580b596656d0fbc71b13ffbcd9
emarino-git <82189750+emarino-git@users.noreply.github.com>
2022-06-24 01:15:03 +0000

    Agregué datos de busqueda

--- src/pages/QRscanner.js
+++ src/pages/QRscanner.js
@@ -3,10 +3,14 @@ import {Fab, TextareaAutosize} from '@material-ui/core'
 import {ArrowBack} from '@material-ui/icons'
 import { Link } from "react-router-dom";
 import QrScan from 'react-qr-reader'
+import { inventario } from '../data/inventario'
 
 function QRscanner() {
 
-    const [qrscan, setQrscan] = useState('No result');
+
+   
+
+    const [qrscan, setQrscan] = useState('Sin resultados');
     const handleScan = data => {
         if (data) {
             setQrscan(data)
@@ -16,8 +20,20 @@ function QRscanner() {
     console.error(err)
     }
 
+    const iterator = inventario.keys();
+    var key = '0'
+
+    for (const keyFind of iterator) {
+        if (inventario[keyFind]['# INVENTARIO'] == qrscan){
+            key = keyFind
+        }
+    }
+
+    console.log(key)
+
+
     return (
-      <div>
+      <>
             <Link to="/">
             <Fab style={{marginRight:10}} color="primary">
                 <ArrowBack/>
@@ -28,7 +44,7 @@ function QRscanner() {
             <center>
             <div style={{marginTop:30}}>
                 <QrScan
-                    delay={300}
+                    delay={200}
                     onError={handleError}
                     onScan={handleScan}
                     style={{ height: 240, width: 320 }}
@@ -36,16 +52,19 @@ function QRscanner() {
             </div>
             </center>
 
-            <TextareaAutosize
-                style={{fontSize:18, width:320, height:100, marginTop:100}}
-                rowsMax={4}
-                defaultValue={qrscan}
-                value={qrscan}
-            />
+            <div className='qr-resultados'>
+                <h3>{qrscan}</h3>
+                <p># INVENTARIO: {qrscan}</p>
+                <p>TIPO DE EQUIPO: {inventario[key]['TIPO DE EQUIPO']}</p>
+                <p>Marca: {inventario[key]['Marca']}</p>
+                <p>Modelo: {inventario[key]['Modelo']}</p>
+                <p>N° Serie: {inventario[key]['N� Serie']}</p>
+                <button className='qr-button'>Registrar Movimiento</button>
+            </div>
+            
 
-      </div>
+      </>
     );
   }
   
   export default QRscanner;
-  
